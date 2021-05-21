941a78ee6120c8d71c788c077d301c8104fdd161
Jeein Kim <jeeink3@hpc3-14-29.local>
2021-05-21 16:29:19 -0700

    added warning to rename the zip files prior to unzipping

--- unzip.py
+++ unzip.py
@@ -1,5 +1,10 @@
 # !/usr/bin/env python3
 # https://askubuntu.com/questions/86849/how-to-unzip-a-zip-file-from-the-terminal
+
+# if you are unzipping the phase folders, prior to unzipping,
+# rename the zip files to phaseX_base or etc.
+# so it does not conflict with the existing folders named phase1, phase2, etc.
+
 import sys
 from zipfile import PyZipFile
 for zip_file in sys.argv[1:]:
